0c7be45ea93c64f3ac0c5fcf5f8990bfded81174
Erick Adrian Perez <erick.perez@plansource.com>
2018-05-01 08:36:59 -0600

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Morbi eu ex vestibulum, congue sapien sed, venenatis mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse interdum leo ligula, at aliquam libero faucibus at. Maecenas commodo varius lectus in imperdiet. In hac habitasse platea dictumst. Maecenas vehicula sagittis mi vel porttitor."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Integer cursus augue eget tortor vulputate rhoncus. Nunc viverra tortor eu lacus tincidunt commodo. Integer faucibus sapien quis commodo venenatis. Donec vitae turpis felis. Curabitur fermentum tortor leo, eu mattis ligula congue a. Quisque efficitur a turpis et faucibus. Sed nibh nisi, fermentum nec tempus ac, varius at arcu. Praesent porttitor risus molestie accumsan aliquet.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x150"
+  )
+end
+
+puts "9 portfolios created"
