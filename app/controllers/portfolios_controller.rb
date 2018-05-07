afef09d4ea25666cedde8404c4c7e93f61012480
Erick Adrian Perez <erick.perez@plansource.com>
2018-05-07 08:16:50 -0600

    Destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    # perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #Destroy/Delete the record
+    @portfolio_item.destroy
+
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was deleted.' }
+    end
+  end
 end
