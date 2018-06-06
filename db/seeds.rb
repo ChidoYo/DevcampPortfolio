471c9a15968842c7647ca4613dbac26291263cb3
Erick Adrian Perez <erick.perez@plansource.com>
2018-06-06 09:09:03 -0600

    Custom Scopes for portfolio-items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Morbi eu ex vestibulum, congue sapien sed, venenatis mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse interdum leo ligula, at aliquam libero faucibus at. Maecenas commodo varius lectus in imperdiet. In hac habitasse platea dictumst. Maecenas vehicula sagittis mi vel porttitor."
+    body: "Morbi eu ex vestibulum, congue sapien sed, venenatis mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse interdum leo ligula, at aliquam libero faucibus at. Maecenas commodo varius lectus in imperdiet. In hac habitasse platea dictumst. Maecenas vehicula sagittis mi vel porttitor.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Integer cursus augue eget tortor vulputate rhoncus. Nunc viverra tortor eu lacus tincidunt commodo. Integer faucibus sapien quis commodo venenatis. Donec vitae turpis felis. Curabitur fermentum tortor leo, eu mattis ligula congue a. Quisque efficitur a turpis et faucibus. Sed nibh nisi, fermentum nec tempus ac, varius at arcu. Praesent porttitor risus molestie accumsan aliquet.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x150"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Integer cursus augue eget tortor vulputate rhoncus. Nunc viverra tortor eu lacus tincidunt commodo. Integer faucibus sapien quis commodo venenatis. Donec vitae turpis felis. Curabitur fermentum tortor leo, eu mattis ligula congue a. Quisque efficitur a turpis et faucibus. Sed nibh nisi, fermentum nec tempus ac, varius at arcu. Praesent porttitor risus molestie accumsan aliquet.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x150"
